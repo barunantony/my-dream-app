58d6da6459cdac83d7cef82d85668e2050657c31
Arun Antony <arun.antony@knab.nl>
2018-11-07 13:53:21 +0100

    use Injectable to inject the LoggingService inside the class ErrorAlertService

--- src/utils/error-alert.service.ts
+++ src/utils/error-alert.service.ts
@@ -1,5 +1,11 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { LoggingService } from "./logging.service";
 
+@Injectable()
 export class ErrorAlertService {
     alertError = new EventEmitter<string>();
+
+    constructor(private log: LoggingService) {
+        this.alertError.subscribe((event) => {log.logMessage(`Event emitted @${Date.now()}: ${event}`)});
+    }
 }
